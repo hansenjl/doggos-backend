9df369465ed5e29abacc77f3660046a7ff7aaa26
hansenjl <jenn.leigh.hansen@gmail.com>
2020-04-08 21:25:51 -0700

    remove likes in the permitted params

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -46,6 +46,6 @@ class DogsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def dog_params
-      params.require(:dog).permit(:url, :likes)
+      params.require(:dog).permit(:url)
     end
 end
